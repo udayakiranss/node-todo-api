899f7639526f17105b3ef747d6eb268e66ece154
udayakiranss <udaykiranss@icloud.com>
2017-06-14 23:29:35 +0530

    Todo Post API

--- server/server.js
+++ server/server.js
@@ -10,8 +10,18 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=> {
-   console.log(req.body);
-});
+    var todo = new Todo({
+        text:req.body.text
+    });
+
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    })
+
+
+    });
 
 
 app.listen(3000, ()=>{
