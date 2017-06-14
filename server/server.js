681aad02800cb59cc5b943f6db1dec364a8ca374
udayakiranss <udaykiranss@icloud.com>
2017-06-14 23:39:09 +0530

    Added GET Todo API

--- server/server.js
+++ server/server.js
@@ -19,10 +19,14 @@ app.post('/todos',(req,res)=> {
     },(err)=>{
         res.status(400).send(err);
     })
-
-
     });
-
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    },(err)=>{
+        res.status(400).send(err);
+    });
+});
 
 app.listen(3000, ()=>{
  console.log('Starting on port 3000');
