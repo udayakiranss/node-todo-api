c275be4bfff878864f854b5e34cdc4e2f4792796
udayakiranss <udaykiranss@icloud.com>
2017-06-16 00:16:01 +0530

    Added GET/:id todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -26,6 +26,22 @@ app.get('/todos',(req,res)=>{
     },(err)=>{
         res.status(400).send(err);
     });
+});
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+             res.status(404).send();
+        }
+        res.send(JSON.stringify(todo,undefined,2));
+    },(err) => {
+        res.status(400).send();
+    });
+    
 });
 
 app.listen(3000, ()=>{
